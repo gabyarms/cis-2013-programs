db157485a23103903730492133a23eb6bdbd2926
gabyarms <70539533+gabyarms@users.noreply.github.com>
2020-09-29 17:48:41 -0500

    Delete mmpg.js

--- mmpg.js
+++
@@ -1,6 +0,0 @@
-var floatMiles = prompt("Enter miles driven");
-floatMiles = parseFloat (miles);    
-var floatGallons = prompt("Enter gallons of gas used");
-floatGallons = parseFloat(gallons); 
-var floatMpg = (floatMiles/floatGallons);
-alert ("Miles per gallon=" + floatMpg); 

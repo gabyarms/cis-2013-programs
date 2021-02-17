d49009eca6763389d1f03e99737e4250c9b4b0d0
gabyarms <70539533+gabyarms@users.noreply.github.com>
2021-02-16 19:26:32 -0600

    Update age.js

--- age.js
+++ age.js
@@ -1,12 +1,10 @@
 var floatAge = prompt("Enter your age");
  floatAge = parseFloat (floatAge);
- var floatDays = (floatAge * 365.25);
+ var floatDays = (floatAge * 365.25).toFixed(2);
   floatDays = parseFloat(floatDays);
  var floatMonths= (floatAge * 12);
   floatMonths = parseFloat(floatMonths);
  var intWeeks= (floatDays / 7);
 var intFortnights = (floatDays /14);
-alert ("approximate months ="+floatMonths);
-alert ("approximate fortnights ="+intFortnights);
-alert ("approximate weeks ="+intWeeks);
-alert ("approximate days ="+floatDays);
+alert ("You are " + floatMonths +" approximate months.\n You are " +intFortnights+ "approximate fortnights.\n You are " +intWeeks+ " approximate weeks.\n You are " + floatDays +
+" approximate days.");
